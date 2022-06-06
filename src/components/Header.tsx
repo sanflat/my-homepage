81fa090b0ccb977a7c69434f25b6aef438ee462c
sanflat <tradeforce0720@gmail.com>
2022-06-06 19:11:29 +0900

    update Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import ThemeToggleButton from './theme-toggle-button'
 import { Box } from '@chakra-ui/react'
 
 export const Header = () => (
-    <Box w='80%' h='1.5rem' align='right'>
+    <Box w='80%' h='1.5rem' textAlign='right'>
       <ThemeToggleButton />
     </Box>
 );
